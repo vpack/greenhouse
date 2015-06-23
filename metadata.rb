bc2e88383d098da0ad89c64c2133dad18ce7ec01
Your Name <you@example.com>
2015-06-23 02:48:23 +0000

    added metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name             'greenhouse'
+maintainer       'VPACK'
+maintainer_email 'VPackiriswamy-consultant@scholastic.com'
+license          'MIT'
+description      'Greenhouse Static Site'
+version          '0.0.1'
+
+%w{ apt nginx }.each do |cookbook|
+  depends cookbook
+end
+ 
+supports 'ubuntu'
+supports 'centos7'
+
