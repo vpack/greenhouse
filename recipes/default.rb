77822d05355fb9dd8b989d4c572da92b1c744436
Your Name <you@example.com>
2015-07-01 20:25:04 +0000

    Jenkins trigger

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,4 +26,7 @@ file '/var/www/nginx-default/index.html' do
   action :create
 end
 
-include_recipe 'greenhouse::datadog'
+node.default['datadog']['api_key'] = "5b74b9ca1c6be82cdb7dcc9f5c9d7446"
+node.default['datadog']['application_key'] = "3735348a84ac0f590fecb0cfc31f2f849726aeb9"
+
+include_recipe 'datadog::dd-agent'
