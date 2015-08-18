18e539b07b852e2f121767c8f583049fba6d4c70
Your Name <you@example.com>
2015-08-18 02:21:44 +0000

    updated consul parameters

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default['consul']['service_mode'] = 'client'
+
+default['consul']['server_ui'] = true
+default['consul']['datacenter'] = "aws-us-east"
+default['consul']['servers'] =  [ "10.44.215.162", "10.44.211.72", "10.44.212.204" ]
+
