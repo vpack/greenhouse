a9029d63f299045efd825af830503742a8eabb3a
Your Name <you@example.com>
2015-08-18 02:34:22 +0000

    updated config

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,7 @@ default['consul']['service_mode'] = 'client'
 
 default['consul']['server_ui'] = true
 default['consul']['datacenter'] = "aws-us-east"
-default['consul']['servers'] =  [ "10.44.215.162", "10.44.211.72", "10.44.212.204" ]
+#default['consul']['servers'] =  [ "10.44.215.162", "10.44.211.72", "10.44.212.204" ]
+default['consul']['servers'] =  %w{10.44.215.162 10.44.211.72 10.44.212.204}
 
+default['consul']['config']['start_join'] = %w{10.44.215.162 10.44.211.72 10.44.212.204}
